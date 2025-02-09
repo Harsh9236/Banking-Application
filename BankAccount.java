aec5baae4048cdc4c3885d2e075dca7caac52083
Harsh <harsh83421@gmail.com>
2025-02-09 21:29:27 +0530

    2025-02-09

--- BankAccount.java
+++ BankAccount.java
@@ -19,18 +19,42 @@ class BankAccount {
         }
     }
 
+    static private boolean doesAccountNumberExist(String accountNumber) {
+        String checkSQL = "SELECT account_number FROM accounts WHERE account_number = ?";
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:accounts.db");
+             PreparedStatement pstmt = conn.prepareStatement(checkSQL)) {
+            pstmt.setString(1, accountNumber);
+            ResultSet rs = pstmt.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            System.err.println("Error checking account number existence: " + e.getMessage());
+            return false;
+        }
+    }
 
     static public void createAccount() {
         Random random = new Random();
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
         Scanner scanner = new Scanner(System.in);
+        boolean accountNumberUnique = false;
 
-        for (int i = 0; i < 8; i++) {
-            int digit = random.nextInt(10);
-            sb.append(digit);
+        while (!accountNumberUnique) {
+            for (int i = 0; i < 8; i++) {
+                int digit = random.nextInt(10);
+                sb1.append(digit);
+            }
+
+            String accountID = sb1.toString();
+
+            if (!doesAccountNumberExist(accountID)) {
+                    accountNumberUnique = true;
+            }
+
+            sb2 = sb1;
         }
 
-        String accountID = sb.toString();
+        String accountID = sb2.toString();
         int initialBalance = 0;
 
         System.out.print("Enter 4 digit passkey: ");
@@ -41,7 +65,7 @@ class BankAccount {
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:accounts.db");
              PreparedStatement pstmt = conn.prepareStatement(insertSQL)) {
             pstmt.setString(1, accountID);
-            pstmt.setInt(2, passKey);
+            pstmt.setString(2, passKey);
             pstmt.setInt(3, 0);
             pstmt.executeUpdate();
 
